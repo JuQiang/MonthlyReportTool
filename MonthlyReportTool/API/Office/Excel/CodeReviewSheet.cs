357f45bf90cc485fa9b7f118d4fb74906b0fe347
lisa <xiaolebin@foxmail.com>
2018-06-12 17:42:40 +0800

    增加审查工作的统计页，重新规划

--- MonthlyReportTool/API/Office/Excel/CodeReviewSheet.cs
+++ MonthlyReportTool/API/Office/Excel/CodeReviewSheet.cs
@@ -39,14 +39,14 @@ namespace MonthlyReportTool.API.Office.Excel
         }
         private void BuildTitle()
         {
-            Utility.BuildFormalSheetTitle(sheet, 2, "B", 2, "F", "代码审查统计分析");
+            Utility.BuildFormalSheetTitle(sheet, 2, "B", 2, "F", "审查工作统计分析");
         }
 
         private int BuildTable(int startRow, List<CodeReviewEntity> list)
         {
             var allbugs = list.GroupBy(cre => cre.CreatedDate2).OrderBy(bugs => bugs.Key).ToList();
 
-            int nextRow = Utility.BuildFormalTable(sheet, startRow, "本迭代代码审查效率", "", "B", "F",
+            int nextRow = Utility.BuildFormalTable(sheet, startRow, "本迭代审查记录单统计", "", "B", "F",
                 new List<string>() { "审查时间", "审查人数", "评审用时（h）", "发现问题数", "效率（个/h）" },
                 new List<string>() { "B,B", "C,C", "D,D", "E,E", "F,F" },
                 allbugs.Count
@@ -72,39 +72,9 @@ namespace MonthlyReportTool.API.Office.Excel
             return nextRow -1;
         }
 
-        private void BuildAnalyzeTable(int startRow)
-        {
-            ExcelInterop.Range tableTitleRange = sheet.Range[sheet.Cells[startRow, "B"], sheet.Cells[startRow, "F"]];
-            Utility.AddNativieResource(tableTitleRange);
-            tableTitleRange.Merge();
-            tableTitleRange.RowHeight = 20;
-            sheet.Cells[startRow, "B"] = "代码审查分析";
-            var tableTitleFont = tableTitleRange.Font;
-            Utility.AddNativieResource(tableTitleFont);
-            tableTitleFont.Bold = true;
-            tableTitleFont.Color = ColorTranslator.ToOle(System.Drawing.Color.Red);
-            tableTitleFont.Size = 12;
-
-            ExcelInterop.Range tableDescriptionRange = sheet.Range[sheet.Cells[startRow + 1, "B"], sheet.Cells[startRow + 1, "P"]];
-            Utility.AddNativieResource(tableDescriptionRange);
-            tableDescriptionRange.Merge();
-            tableDescriptionRange.RowHeight = 20;
-            sheet.Cells[startRow + 1, "B"] = "说明：针对本迭代做的代码审查工作做分析";
-
-            ExcelInterop.Range descRange = sheet.Range[sheet.Cells[startRow + 2, "B"], sheet.Cells[startRow + 10, "F"]];
-            descRange.HorizontalAlignment = ExcelInterop.XlHAlign.xlHAlignLeft;
-            descRange.VerticalAlignment = ExcelInterop.XlVAlign.xlVAlignTop;
-            Utility.AddNativieResource(descRange);
-            descRange.Merge();
-
-            ExcelInterop.Borders descBorder = descRange.Borders;
-            Utility.AddNativieResource(descBorder);
-            descBorder.LineStyle = ExcelInterop.XlLineStyle.xlContinuous;
-        }
-
         private int BuildCodeReviewTable(int startRow, List<BugEntity> list)
         {
-            int nextRow = Utility.BuildFormalTable(this.sheet, startRow, "本迭代代码评审发现问题统计", "说明：", "B", "P",
+            int nextRow = Utility.BuildFormalTable(this.sheet, startRow, "本迭代审查发现问题统计", "说明：", "B", "P",
                 new List<string>() { "BugID", "关键应用", "模块", "功能","缺陷发现方式", "问题类别", "严重级别", "Bug标题", "指派给", "发现人" },
                 new List<string>() { "B,B", "C,D", "E,F", "G,H", "I,I", "J,J","K,K", "L,N", "O,O", "P,P" },
                 list.Count);
@@ -133,6 +103,36 @@ namespace MonthlyReportTool.API.Office.Excel
             Utility.SetCellAlignAndWrap(sheet.Range[sheet.Cells[startRow, "B"], sheet.Cells[startRow + orderedBugs.Count - 1, "B"]]);
 
             return nextRow-1;
+        }
+
+        private void BuildAnalyzeTable(int startRow)
+        {
+            ExcelInterop.Range tableTitleRange = sheet.Range[sheet.Cells[startRow, "B"], sheet.Cells[startRow, "F"]];
+            Utility.AddNativieResource(tableTitleRange);
+            tableTitleRange.Merge();
+            tableTitleRange.RowHeight = 20;
+            sheet.Cells[startRow, "B"] = "审查工作分析";
+            var tableTitleFont = tableTitleRange.Font;
+            Utility.AddNativieResource(tableTitleFont);
+            tableTitleFont.Bold = true;
+            tableTitleFont.Color = ColorTranslator.ToOle(System.Drawing.Color.Red);
+            tableTitleFont.Size = 12;
+
+            ExcelInterop.Range tableDescriptionRange = sheet.Range[sheet.Cells[startRow + 1, "B"], sheet.Cells[startRow + 1, "P"]];
+            Utility.AddNativieResource(tableDescriptionRange);
+            tableDescriptionRange.Merge();
+            tableDescriptionRange.RowHeight = 20;
+            sheet.Cells[startRow + 1, "B"] = "说明：针对本迭代的审查工作做分析";
+
+            ExcelInterop.Range descRange = sheet.Range[sheet.Cells[startRow + 2, "B"], sheet.Cells[startRow + 10, "F"]];
+            descRange.HorizontalAlignment = ExcelInterop.XlHAlign.xlHAlignLeft;
+            descRange.VerticalAlignment = ExcelInterop.XlVAlign.xlVAlignTop;
+            Utility.AddNativieResource(descRange);
+            descRange.Merge();
+
+            ExcelInterop.Borders descBorder = descRange.Borders;
+            Utility.AddNativieResource(descBorder);
+            descBorder.LineStyle = ExcelInterop.XlLineStyle.xlContinuous;
         }
     }
 }
---
+++ MonthlyReportTool/API/TFS/WorkItem/WorkReview.cs
@@ -0,0 +1,131 @@
+﻿using MonthlyReportTool.API.TFS.Agile;
+using System;
+using System.Collections.Generic;
+
+namespace MonthlyReportTool.API.TFS.WorkItem
+{
+    public class WorkReview
+    {
+        //共享查询%2F迭代总结数据查询%2F15%20提交单统计分析%2F00本迭代_提交单总数
+
+        private static List<WorkReviewEntity> GetCommitmentListByIteration(string project, IterationEntity ite, string query, List<string> columns, List<string> values)
+        {
+            List<WorkReviewEntity> list = new List<WorkReviewEntity>();
+            string wiql = API.TFS.Utility.GetQueryClause(query);
+            wiql = API.TFS.Utility.ReplaceInformationFromWIQLByProject(wiql, columns);
+
+            string sql = String.Format(wiql, values.ToArray());
+
+            string responseBody = Utility.ExecuteQueryBySQL(sql);
+            var commitments = Utility.ConvertWorkitemFlatQueryResult2Array(responseBody);
+            foreach (var commitment in commitments)
+            {
+                list.Add(
+                    new WorkReviewEntity()
+                    {
+                        Id = Convert.ToInt32(commitment["fields"]["System.Id"]),
+                        KeyApplicationName = Convert.ToString(commitment["fields"]["Teld.Scrum.KeyApplicationName"]),
+                        ModulesName = Convert.ToString(commitment["fields"]["Teld.Scrum.ModulesName"]),
+                        FuncName = Convert.ToString(commitment["fields"]["Teld.Scrum.FuncName"]),
+                        SubmitType = Convert.ToString(commitment["fields"]["Teld.Scrum.Worklog.SubmitLog.SubmitType"]),
+                        Title = Convert.ToString(commitment["fields"]["System.Title"]),
+                        State = Convert.ToString(commitment["fields"]["System.State"]),
+                        AssignedTo = Convert.ToString(commitment["fields"]["System.AssignedTo"]),
+                        TestResponsibleMan = Convert.ToString(commitment["fields"]["Teld.Scrum.TestResponsibleMan"]),
+                        SubmitUser = Convert.ToString(commitment["fields"]["Teld.Scrum.Worklog.SubmitLog.SubmitUser"]),
+                        BackNum = Convert.ToInt32(commitment["fields"]["Teld.Scrum.Worklog.SubmitLog.BackNum"]),
+                        IsNeedPerformanceTest = (Convert.ToString(commitment["fields"]["System.AssignedTo"])) == "是",
+                        TestFinishedTime = Convert.ToString(commitment["fields"]["Teld.Scrum.TestFinishedTime"]),
+                        SubmitDate = Convert.ToString(commitment["fields"]["Teld.Scrum.Worklog.SubmitLog.SubmitDate"]),
+                        PlanTestFinishedTime = Convert.ToString(commitment["fields"]["Teld.Scrum.Backlog.PlanTestFinishedTime"]),
+                        AcceptTime = Convert.ToString(commitment["fields"]["Teld.Scrum.Backlog.AcceptTime"]),
+                        CreatedDate = Convert.ToString(commitment["fields"]["System.CreatedDate"]),
+                        BackType = Convert.ToString(commitment["fields"]["Teld.Scrum.Worklog.SubmitLog.BackType"]),
+                        IterationPath = Convert.ToString(commitment["fields"]["System.IterationPath"]),
+                        SubmitNumberOfTime = Convert.ToInt32(commitment["fields"]["Teld.Scrum.SubmitNumberOfTime"]),
+                        TeamProject = Convert.ToString(commitment["fields"]["System.TeamProject"]),
+                        FindedBugCount = Convert.ToInt32(commitment["fields"]["Teld.Scrum.FindedBugCount"]),
+                    }
+                );
+            }
+            return list;
+        }
+
+        public static List<List<WorkReviewEntity>> GetAll(string project, IterationEntity ite)
+        {
+            List<List<WorkReviewEntity>> list = new List<List<WorkReviewEntity>>();
+
+            var all = GetCommitmentListByIteration(project, ite, "共享查询%2F迭代总结数据查询%2F15%20提交单统计分析%2F00本迭代_提交单总数",
+                new List<string>() { "[System.TeamProject] =","[Teld.Scrum.RemovedDate] >=", "[Teld.Scrum.RemovedDate] <", "[Teld.Scrum.TestFinishedTime] >=", "[Teld.Scrum.TestFinishedTime] <", "[System.CreatedDate] <" },
+                new List<string>(){
+                    project,
+                    DateTime.Parse(ite.StartDate).AddDays(0).ToString("yyyy-MM-dd HH:mm:ss.fff"),//第一天是大于等于
+                    DateTime.Parse(ite.EndDate).AddDays(1).ToString("yyyy-MM-dd HH:mm:ss.fff"),//最后一天要加一
+                    DateTime.Parse(ite.StartDate).AddDays(0).ToString("yyyy-MM-dd HH:mm:ss.fff"),//第一天是大于等于
+                    DateTime.Parse(ite.EndDate).AddDays(1).ToString("yyyy-MM-dd HH:mm:ss.fff"),//最后一天要加一
+                    DateTime.Parse(ite.EndDate).AddDays(1).ToString("yyyy-MM-dd HH:mm:ss.fff"),//最后一天要加一
+                }
+            );
+            var testpassed = GetCommitmentListByIteration(project, ite, "共享查询%2F迭代总结数据查询%2F15%20提交单统计分析%2F05本迭代_提交单测试通过总数",
+                    new List<string>() { "[System.TeamProject] =", "[Teld.Scrum.TestFinishedTime] >=", "[Teld.Scrum.TestFinishedTime] <" },
+                new List<string>(){
+                    project,
+                    DateTime.Parse(ite.StartDate).AddDays(0).ToString("yyyy-MM-dd HH:mm:ss.fff"),//第一天是大于等于
+                    DateTime.Parse(ite.EndDate).AddDays(1).ToString("yyyy-MM-dd HH:mm:ss.fff"),//最后一天要加一
+                }
+            );
+
+            var removed = GetCommitmentListByIteration(project, ite, "共享查询%2F迭代总结数据查询%2F15%20提交单统计分析%2F10本迭代_已移除提交单总数",
+                new List<string>() { "[System.TeamProject] =", "[Teld.Scrum.RemovedDate] >=", "[Teld.Scrum.RemovedDate] <"},
+                new List<string>(){
+                    project,
+                    DateTime.Parse(ite.StartDate).AddDays(0).ToString("yyyy-MM-dd HH:mm:ss.fff"),//第一天是大于等于
+                    DateTime.Parse(ite.EndDate).AddDays(1).ToString("yyyy-MM-dd HH:mm:ss.fff"),//最后一天要加一
+                }
+            );
+            var needperf = GetCommitmentListByIteration(project, ite, "共享查询%2F迭代总结数据查询%2F15%20提交单统计分析%2F15本迭代_需性能测试提交单总数",
+                    new List<string>() { "[System.TeamProject] =", "[System.CreatedDate] <", "[Teld.Scrum.TestFinishedTime] >=" },
+                new List<string>(){
+                    project,
+                    DateTime.Parse(ite.EndDate).AddDays(1).ToString("yyyy-MM-dd HH:mm:ss.fff"),//最后一天要加一
+                    DateTime.Parse(ite.StartDate).AddDays(0).ToString("yyyy-MM-dd HH:mm:ss.fff"),//第一天是大于等于
+                }
+            );
+            var perftestpassed = GetCommitmentListByIteration(project, ite, "共享查询%2F迭代总结数据查询%2F15%20提交单统计分析%2F20本迭代_性能测试通过提交单总数",
+                new List<string>() { "[System.TeamProject] =", "[Teld.Scrum.TestFinishedTime] >=", "[Teld.Scrum.TestFinishedTime] <" },
+                new List<string>(){
+                    project,
+                    DateTime.Parse(ite.StartDate).AddDays(0).ToString("yyyy-MM-dd HH:mm:ss.fff"),//第一天是大于等于
+                    DateTime.Parse(ite.EndDate).AddDays(1).ToString("yyyy-MM-dd HH:mm:ss.fff"),//最后一天要加一
+                }
+            );
+
+            var failed = GetCommitmentListByIteration(project, ite, "共享查询%2F迭代总结数据查询%2F15%20提交单统计分析%2F30本迭代_打回过提交单总数",
+                new List<string>() { "[System.TeamProject] =", "[Teld.Scrum.TestFinishedTime] >=", "[Teld.Scrum.TestFinishedTime] <" },
+                new List<string>(){
+                    project,
+                    DateTime.Parse(ite.StartDate).AddDays(0).ToString("yyyy-MM-dd HH:mm:ss.fff"),//第一天是大于等于
+                    DateTime.Parse(ite.EndDate).AddDays(1).ToString("yyyy-MM-dd HH:mm:ss.fff"),//最后一天要加一
+                }
+            );
+            var longtime = GetCommitmentListByIteration(project, ite, "共享查询%2F迭代总结数据查询%2F15%20提交单统计分析%2F30本迭代_提交单持续时间过长总数",
+                new List<string>() { "[System.TeamProject] =", "[Teld.Scrum.TestFinishedTime] >=", "[Teld.Scrum.TestFinishedTime] <" },
+                new List<string>(){
+                    project,
+                    DateTime.Parse(ite.StartDate).AddDays(0).ToString("yyyy-MM-dd HH:mm:ss.fff"),//第一天是大于等于
+                    DateTime.Parse(ite.EndDate).AddDays(1).ToString("yyyy-MM-dd HH:mm:ss.fff"),//最后一天要加一
+                }
+            );
+
+            list.Add(all);
+            list.Add(testpassed);
+            list.Add(removed);
+            list.Add(needperf);
+            list.Add(perftestpassed);
+            list.Add(failed);
+            list.Add(longtime);
+
+            return list;
+        }
+    }
+}
